ab071765450d8a5e034ccfc263ec115c27b8c0bc
Sergey <sviridov.sv82@gmail.com>
2018-03-21 16:50:08 +0200

    add ajax for comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,31 @@
 class CommentsController < ApplicationController
+  def create
+    @comment = Comment.new(comment_params)
+    @comment.post_id = params[:post_id]
+
+    if @comment.save
+      respond_to do |f|
+        f.html { redirect_to post_path(@comment.post) }
+        f.js
+      end
+    else
+      redirect_to post_path(params[:post_id]), notice: 'Comment was not saved'
+    end
+  end
+
+  def destroy
+    @comment = current_user.comments.find(params[:id])
+    @comment.destroy
+
+    respond_to do |f|
+      f.html { redirect_to post_path(params[:post_id]) }
+      f.js
+    end
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:body, :user_id, :post_id)
+  end
 end
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,8 @@ module PostsHelper
   def post_author(post)
     post.user.login
   end
+
+  def comment_author(comment)
+    comment.user.login
+  end
 end

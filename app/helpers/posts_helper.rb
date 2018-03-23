2785a9ed3cb8ccfb3ab72cf667884873e9cdd94b
Sergey <sviridov.sv82@gmail.com>
2018-03-23 10:51:13 +0200

    show textile text word by word

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -6,4 +6,25 @@ module PostsHelper
   def comment_author(comment)
     comment.user.login
   end
+
+  def render_haml(code)
+    engine = Haml::Engine.new(code)
+    engine.render
+  end
+
+  def array_of_words(text)
+    text.split(' ')
+  end
+
+  def textile_word(word)
+    return render_haml(textilize(word)) if textile_text(@post.text).include?(word)
+    word
+  end
+
+  private
+
+  def textile_text(text)
+    t = text.split(' ') - RedCloth.new(text).to_html.split(' ')
+    t.empty? ? false : t
+  end
 end
